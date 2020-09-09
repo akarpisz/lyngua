1cdfc6a19d72b47d3e4d429c6ee3031efb0856d5
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-09-08 19:48:28 -0600

    changing server for deployment

--- index.js
+++ index.js
@@ -10,28 +10,28 @@ const PORT = process.env.PORT || 5001;
 //const passportJWT = require('passport-jwt');
 
 require("dotenv").config(path.join(__dirname, "/.env"));
-const cors = require('cors');
+const cors = require("cors");
 
 
-mongoose.set('useCreateIndex', true)
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lyngua")
+mongoose.set("useCreateIndex", true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lyngua");
 
 // app.use(passport.initialize());
 // app.use(cors());
 
 app.use(cors({
-    origin: process.env.ALLOW_ORIGIN,
-    credentials: true,
-    allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
-    methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS'
-  }))
+	origin: process.env.ALLOW_ORIGIN,
+	credentials: true,
+	allowedHeaders: "X-Requested-With, Content-Type, Authorization",
+	methods: "GET, POST, PATCH, PUT, POST, DELETE, OPTIONS"
+}));
 
 //change to client/build for production
-app.use(express.static(path.join(__dirname, "client/public")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(logger("dev"));
@@ -39,5 +39,5 @@ app.use("/api", api);
 
 
 app.listen(PORT, ()=>{
-    console.log(`App started, server listening on port ${PORT}`);
-})
+	console.log(`App started, server listening on port ${PORT}`);
+});
